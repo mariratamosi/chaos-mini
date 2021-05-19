e1fe98d13cdab1f6edf5b0627dfa5928ed46be5d
marira <rongrazzo@gmail.com>
2021-05-19 23:14:14 +0600

    added login:: check if logged in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import Login from "./Modules/login/component/login";
-import FeelingsList from "./Modules/feelingsDesc/component/feelingList";
-
+import { CheckAuth } from "./Service/Auth";
+import { useEffect } from "react";
+import React from "react";
+import { useSelector } from "react-redux";
 import "./Styles/App.scss";
 
 function App() {
+  const userInfo = useSelector((state) => state.user);
+  console.log(userInfo);
+  useEffect(() => {
+    let isLoggedIn = CheckAuth(userInfo);
+    console.log(isLoggedIn);
+  }, []);
+
   return <div className="App"></div>;
 }
 
---
+++ src/Service/Auth.js
@@ -0,0 +1,3 @@
+export const CheckAuth = (userInfo) => {
+  return userInfo.isLoggedIn;
+};
