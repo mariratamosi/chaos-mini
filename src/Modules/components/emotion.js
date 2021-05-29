066195213ed6aaea69eec2f02334f0c22e12d226
marira <rongrazzo@gmail.com>
2021-05-29 22:10:05 +0600

    emotion click event with dom manipulation

--- src/Modules/components/emotion.js
+++ src/Modules/components/emotion.js
@@ -1,3 +1,16 @@
-export const Emotion = ({ name }) => {
-  return <div className="single-emotion">{name}</div>;
+export const Emotion = ({ name, id }) => {
+  const addToSelection = (e) => {
+    console.log(e.target.dataset.id);
+    let allEmotions = document.querySelectorAll(".single-emotion");
+    console.log(allEmotions);
+    allEmotions.forEach((element) => {
+      element.classList.remove("active");
+    });
+    e.target.classList.add("active");
+  };
+  return (
+    <div className="single-emotion" data-id={id} onClick={addToSelection}>
+      {name}
+    </div>
+  );
 };
--- src/Modules/feelingsDesc/component/feelingList.js
+++ src/Modules/feelingsDesc/component/feelingList.js
@@ -1,17 +1,22 @@
 import { Logout } from "modules/logout/component/logout";
 import { useSelector } from "react-redux";
 import { Emotion } from "modules/components";
+import "styles/feelingsDesc.scss";
+import { Link } from "react-router-dom";
 
 export const FeelingsList = () => {
   const userInfo = useSelector((state) => state.user);
   return (
     <div className="feelings-desc-container">
       <h1>Hello {userInfo.userName}, how are you feeling today?</h1>
-      <Emotion name="Angry" />
-      <Emotion name="Afraid" />
-      <Emotion name="Sad" />
-      <Emotion name="Disgust" />
-      <Emotion name="Happy" />
+      <Emotion name="Angry" id="1" />
+      <Emotion name="Afraid" id="2" />
+      <Emotion name="Sad" id="3" />
+      <Emotion name="Disgust" id="4" />
+      <Emotion name="Happy" id="5" />
+      <Link to="/login" className="link">
+        Continue
+      </Link>
       <Logout />
     </div>
   );
