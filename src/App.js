2358e966c9f2f1fb2571d76a25f2a74c3398fe62
marira <rongrazzo@gmail.com>
2021-05-29 21:04:09 +0600

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { CheckAuth } from "service";
 import { useEffect } from "react";
 import React from "react";
-import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { addUsernameToStore } from "redux/Actions";
 import Routes from "routeConfig";
 
 function App() {
-  const userInfo = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    console.log("app mounted");
+    console.log("app started");
 
     let loginData = CheckAuth();
 
@@ -22,7 +20,7 @@ function App() {
 
   return (
     <div className="App">
-      <Routes loggedIn={userInfo.isLoggedIn} />
+      <Routes />
     </div>
   );
 }
