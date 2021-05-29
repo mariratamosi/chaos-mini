1d16659a126d4549d2dd9ae2a7805de471d1e54a
marira <rongrazzo@gmail.com>
2021-05-29 20:06:15 +0600

    logout :: done

--- src/Modules/exc/component/exc.js
+++ src/Modules/exc/component/exc.js
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 
-export const Exc = () => {
+export const Exc = (props) => {
+  const { state = {} } = props.location;
+  const { error } = state;
   return (
     <div>
-      <h1>i am a public page</h1>
+      <h1>Chaos mini</h1>
+      {error && <div>ERROR: {error}</div>}
       <Link to="/login" className="link">
         Login
       </Link>
